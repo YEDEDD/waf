cd9d8b2a4e33e9ac6f760c99276b7b61e513d152
YEDEDD <94660330+YEDEDD@users.noreply.github.com>
2024-08-22 14:54:53 +0800

    Update access.lua

--- nginx/waf/access.lua
+++ nginx/waf/access.lua
@@ -3,9 +3,6 @@ require 'init'
 -- 从 Nginx 变量中获取每个server规则目录路径
 config_rule_dir = ngx.var.rule_config_dir
 
--- 创建日志目录
-
-
 function waf_main()
     if white_ip_check() then
     elseif black_ip_check() then
@@ -19,8 +16,6 @@ function waf_main()
     --elseif post_attack_check() then
     elseif waf_deny_no_match() then
     else
-        -- 如果没有匹配到任何规则，记录未被拦截的请求
-        --log_record_no_match('No_Match', ngx.var.request_uri, "_", "No_Match")
         return
     end
 end
